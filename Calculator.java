a76ada0bf7adb5e3cb9907c1c65e2fbe6a0715f2
s9202259 <s9202259@yahoo.com.tw>
2016-04-24 23:22:25 +0800

    final add
    finish the program

--- Calculator.java
+++ Calculator.java
@@ -10,7 +10,7 @@ import javafx.scene.Parent;
 import java.util.*;
 
 public class Calculator extends Application {
- 
+
 	public ArrayList<Double> number = new ArrayList<Double>();
 	public ArrayList<String> numberShow = new ArrayList<String>();
 	public String all = "";
@@ -22,17 +22,11 @@ public class Calculator extends Application {
 	public boolean hasIndex = false;
 	
 	@Override
-	public void start(Stage primaryStage){		
-		Menu V= new Menu("檢視(V)");
-		MenuBar menu = new MenuBar();
-		MenuItem calculator = new MenuItem("計算機");
-		
-		menu.getMenus().addAll(V);
-		V.getItems().addAll(calculator);
-		
+	public void start(Stage primaryStage) {
 		TextField textField = new TextField();
 		textField.setAlignment(Pos.CENTER_RIGHT);
 		textField.setEditable(false);
+		textField.setText("0");
 				
 		Button b0 = new Button("0");
 		b0.setOnAction(e -> {
@@ -114,18 +108,20 @@ public class Calculator extends Application {
 
 		Button bP = new Button("+");
 		bP.setOnAction(e -> {	
-			if (hasIndex == false) {
+			if(hasIndex == false) {
 				sum = Double.valueOf(calculate);
 				hasIndex = true;
 			}
 			else {
 				if (all.charAt(all.length() - 1) == index) {
-					all = all.substring(0, display.length() - 1);
+					all = all.substring(0, all.length() - 1);
 				}
 				else {
+					hold();
+					calculate();
 				}	
 			}			
-			all += "+";
+			all += " + ";
 			calculate += "+";
 			index = '+';
 			textField.setText(all);
@@ -139,12 +135,14 @@ public class Calculator extends Application {
 			}
 			else {
 				if (all.charAt(all.length() - 1) == index) {
-					all = display.substring(0,display.length() - 1);
+					all = all.substring(0, all.length() - 1);
 				}
 				else {
+					hold();
+					calculate();
 				}
 			}				
-			all += "-";
+			all += " - ";
 			calculate += "-";
 			index = '-';
 			textField.setText(all);
@@ -154,15 +152,17 @@ public class Calculator extends Application {
 		bC.setOnAction(e -> {
 			if (hasIndex == false) {
 				sum = Double.valueOf(calculate);
-				hasindex = true;
+				hasIndex = true;
 			}
 			else {
 				if(all.charAt(all.length() - 1) == index)
 					all = all.substring(0, all.length() - 1);
 				else{
+					hold();
+					calculate();
 				}
 			}			
-			all += "x";
+			all += " x ";
 			calculate += "x";
 			index = 'x';
 			textField.setText(all);
@@ -176,49 +176,52 @@ public class Calculator extends Application {
 			}
 			else {
 				if (all.charAt(all.length() - 1) == index)
-					all = all.substring(0,display.length() - 1);
+					all = all.substring(0, all.length() - 1);
 				else {
+					hold();
+					calculate();
 				}
 			}			
-			all += "/";
-			calculate += "/";
-			index = '/';
+			all += " ÷ ";
+			calculate += "÷";
+			index = '÷';
 			textField.setText(all);
 		});
 
 		Button bDel = new Button("del");
 		bDel.setOnAction(e -> {
-			all = all.substring(0, display.length() - 1);
-			calculate = calculate.substring(0, compute.length() - 1);
+			all = all.substring(0, all.length() - 1);
+			calculate = calculate.substring(0, calculate.length() - 1);
 			textField.setText(all);
 		});
 
 		Button bCE = new Button("CE");
 		bCE.setOnAction(e -> {
 			if (hasIndex == true) {
-				all = all.substring(0, display.length() - display.lastIndexOf(index));
-				calculate = calculate.substring(0, compute.length() - compute.lastIndexOf(index));
+				all = all.substring(0, all.length() - all.lastIndexOf(index));
+				calculate = calculate.substring(0, calculate.length() - calculate.lastIndexOf(index));
 			}	
 			else {
 				all = "";
 				calculate = "";
 			}
-			textField.setText(all);
+			textField.setText("0");
 		});
 
 		Button bc = new Button("C");
-		bCc.setOnAction(e -> {
+		bc.setOnAction(e -> {
 			all = "";
 			calculate = "";
 			sum = 0;
 			hold = "";
 			index = '0';
 			hasIndex = false;				
-			textField.setText(all);
+			textField.setText("0");
 		});
-		
+
 		Button bPM = new Button("±");
-		bPM.setOnAction(e -> {		
+		bPM.setOnAction(e -> {
+			hold();
 			if (index == '0') {
 				calculate();
 				sum *= (-1);
@@ -226,12 +229,12 @@ public class Calculator extends Application {
 				calculate = all;
 				textField.setText(all);
 			}
-			hold = "";						
+			hold = "";		
 		});
 
 		Button bE = new Button("=");
-		bE.setOnAction(e -> {				
-			setRemember();
+		bE.setOnAction(e -> {
+			hold();
 			calculate();			
 			all = String.valueOf(sum);
 			calculate = all;
@@ -239,5 +242,94 @@ public class Calculator extends Application {
 			hasIndex = false;				
 			textField.setText(all);
 		});
+		
+		b0.setPrefSize(62, 30);
+		b1.setPrefSize(62, 30);
+		b2.setPrefSize(62, 30);
+		b3.setPrefSize(62, 30);
+		b4.setPrefSize(62, 30);
+		b5.setPrefSize(62, 30);
+		b6.setPrefSize(62, 30);
+		b7.setPrefSize(62, 30);
+		b8.setPrefSize(62, 30);
+		b9.setPrefSize(62, 30);
+		bDot.setPrefSize(62, 30);
+		bP.setPrefSize(62, 30);
+		bM.setPrefSize(62, 30);
+		bC.setPrefSize(62, 30);
+		bD.setPrefSize(62, 30);
+		bDel.setPrefSize(62, 30);
+		bCE.setPrefSize(62, 30);
+		bc.setPrefSize(62, 30);
+		bPM.setPrefSize(62, 30);
+		bE.setPrefSize(62, 30);
+
+		StackPane paneTextField = new StackPane();
+		Pane pane = new VBox();
+		HBox first = new HBox();
+		HBox second = new HBox();
+		HBox third = new HBox();
+		HBox fourth = new HBox();
+		HBox fifth = new HBox();
+
+		first.getChildren().addAll(bDel, bCE, bc, bPM);
+		second.getChildren().addAll(b7, b8, b9, bD);
+		third.getChildren().addAll(b4, b5, b6, bC);
+		fourth.getChildren().addAll(b1, b2, b3, bM);
+		fifth.getChildren().addAll(bDot, b0, bP, bE);
+		
+
+		paneTextField.getChildren().add(textField);
+		pane.getChildren().addAll(paneTextField, first, second,
+				third, fourth, fifth);			
+		
+		Scene scene = new Scene(pane, 250, 250);
+		
+		primaryStage.setTitle("U10416030 計算機");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public void hold() {			
+		for(int i = 1 + calculate.lastIndexOf(index); i < calculate.length(); i++){
+			hold += calculate.charAt(i);	
+		}			
+	}
+	
+	public void calculate() {		
+		switch(index) {
+			case '+':				
+				sum += Double.valueOf(hold);
+				break;
+			case '-':
+				sum -= Double.valueOf(hold);
+				break;
+			case 'x':
+				sum *= Double.valueOf(hold);
+				break;
+			case '÷':
+				sum /= Double.valueOf(hold);
+				break;
+			case '0':
+				sum = Double.valueOf(hold);
+				break;
+		}
+		hold = "";		
+	}
+	
+	public void calculateSum(){
+		holdSum = number.get(0);
+		for(int i = 0; i < numberShow.size(); i++){
+			if (numberShow.get(i) == "+") {
+				holdSum += number.get(i + 1);
+			}
+			else {
+				holdSum -= number.get(i + 1);
+			}				
+		}
+	}
+
+	public static void main(String[] args) {
+		launch();
 	}
 }
